79fcf9845f6ad201e29e6ca3c886a4327f7c057a
fenglinglwb <fenglinglwb@gmail.com>
2022-09-22 10:33:43 +0800

    fix a bug

--- generate_image.py
+++ generate_image.py
@@ -98,7 +98,8 @@ def generate_images(
     device = torch.device('cuda')
     with dnnlib.util.open_url(network_pkl) as f:
         G_saved = legacy.load_network_pkl(f)['G_ema'].to(device).eval().requires_grad_(False) # type: ignore
-    G = Generator(z_dim=512, c_dim=0, w_dim=512, img_resolution=resolution, img_channels=3).to(device).eval().requires_grad_(False)
+    net_res = 512 if resolution > 512 else resolution
+    G = Generator(z_dim=512, c_dim=0, w_dim=512, img_resolution=net_res, img_channels=3).to(device).eval().requires_grad_(False)
     copy_params_and_buffers(G_saved, G, require_all=True)
 
     os.makedirs(outdir, exist_ok=True)
